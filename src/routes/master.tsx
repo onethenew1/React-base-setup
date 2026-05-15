df52f727bfd5f9c312c8c2cf013a275d88d37acc
DevPatelWork <dev@miraclecloud-technology.com>
2026-05-15 17:11:22 +0530

    fix: format code for consistency and improve readability in routeTree and master components

--- src/routes/master.tsx
+++ src/routes/master.tsx
@@ -14,8 +14,24 @@ function RouteComponent() {
   return (
     <div>
       <h1>Get to know iPhone</h1>
-      <div>
-        <Appleslide />
+      <Appleslide />
+      <div className="flex items-center justify-center">
+        <div
+          onClick={() => console.log("Outer")}
+          className="mt-4 rounded-lg border border-blue-300 bg-blue-200 p-4"
+        >
+          <div
+            onClick={() => console.log("Middle")}
+            className="mt-4 flex items-center justify-center rounded-md border border-pink-300 bg-pink-200 p-4"
+          >
+            <button
+              onClick={() => console.log("Inner")}
+              className="mt-4 rounded-md border border-yellow-300 bg-yellow-200 p-4"
+            >
+              Click
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
